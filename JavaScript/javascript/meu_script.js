b1bfe652a8e7dbe31205b185029b7e83275f50c8
Genimarcio <genimarcio.silva@gmail.com>
2021-08-03 22:16:56 -0300

    Curso Desenvolvimento Web Completo - V1

---
+++ JavaScript/javascript/meu_script.js
@@ -0,0 +1,7 @@
+//alert('Ola, Seja bem vindo ao curso!')
+
+/*
+    selecionar o elemento no DOM
+    atualizar o valor desse elemento com uma string
+*/
+document.getElementById('nome').value='Oi'
---
+++ Primeiro projeto/Aula 299-Index.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <meta chaset="utf-8">
+        <title>Curso PHP</title>
+    </head>
+
+    <body>
+        <?php>
+            echo'Utilizando a tag padrão';
+        ?>
+
+
+    </body>
+
+</html>
